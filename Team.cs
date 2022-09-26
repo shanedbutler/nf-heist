b2c120fe721116214135dde5c6694e2fe3de8c91
shanedbutler <shanedbutler@gmail.com>
2022-09-26 10:47:34 -0400

    Add skill level and luck properties to Team class

--- Team.cs
+++ Team.cs
@@ -13,5 +13,8 @@ namespace heist
         {
             this.Mates.Add(mate);
         }
+        public int SkillTotal { get; set;}
+        private static Random rnd = new Random();
+        public int Luck {get; set;} = rnd.Next(-10, 10);
     }
 }
