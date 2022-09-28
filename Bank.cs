2531c2b7484f2da3eb0a78adf4f156445f6e3a14
shanedbutler <shanedbutler@gmail.com>
2022-09-28 13:28:29 -0400

    Add class properties

--- Bank.cs
+++ Bank.cs
@@ -1,8 +1,19 @@
 namespace heist
 {
-    class Bank
+    public class Bank
     {
-        public int Difficulty {get; set;}
+        public int Difficulty { get; set; }
+        public int CashOnHand { get; set; }
+        public int AlarmScore { get; set; }
+        public int VaultScore { get; set; }
+        public int SecurityGuardScore { get; set; }
+        public bool IsSecure
+        {
+            get
+            {
+                return AlarmScore <= 0 && VaultScore <= 0 && SecurityGuardScore <= 0 ? false : true;
+            }
+        }
         public Bank(int userDifficulty)
         {
             this.Difficulty = userDifficulty;
