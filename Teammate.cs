7fa287c64a94c7fe0722a2b12855c19c61260246
shanedbutler <shanedbutler@gmail.com>
2022-09-29 09:58:51 -0400

    Add specialist property to class

--- Teammate.cs
+++ Teammate.cs
@@ -2,13 +2,15 @@ namespace heist
 {
     public class Teammate
     {
-        public string Name {get;}
-        public int Skill {get;}
-        public decimal Courage {get;}
+        public string Name { get; }
+        public int Skill { get; }
+        public decimal Courage { get; }
+        public string Specialty { get; }
 
-        public Teammate(string name, int skill, decimal courage)
+        public Teammate(string name, string specialty, int skill, decimal courage)
         {
             this.Name = name;
+            this.Specialty = specialty;
             this.Skill = skill;
             this.Courage = courage;
             PrintInfo();
@@ -16,6 +18,7 @@ namespace heist
         public void PrintInfo()
         {
             Console.WriteLine($"\n-~-~-{Name}-~-~-");
+            Console.WriteLine($"Specialty: {Specialty}");
             Console.WriteLine($"Skill Level: {Skill}");
             Console.WriteLine($"Courage Factor: {Courage}");
         }
