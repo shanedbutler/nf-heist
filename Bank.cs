dd2f6e1d8ad30eb05ee5b9e51e219b20e2ae0204
shanedbutler <shanedbutler@gmail.com>
2022-10-03 12:10:10 -0400

    Add bank report, randomize initial property values

--- Bank.cs
+++ Bank.cs
@@ -2,11 +2,11 @@ namespace heist
 {
     public class Bank
     {
-        public int Difficulty { get; set; }
-        public int CashOnHand { get; set; }
-        public int AlarmScore { get; set; }
-        public int VaultScore { get; set; }
-        public int SecurityGuardScore { get; set; }
+        public int Difficulty { get; } 
+        public int CashOnHand { get; set; } = new Random().Next(50000, 1000000);
+        public int AlarmScore { get; set; } = new Random().Next(0, 100);
+        public int VaultScore { get; set; } = new Random().Next(0, 100);
+        public int SecurityGuardScore { get; set; } = new Random().Next(0, 100);
         public bool IsSecure
         {
             get
@@ -17,6 +17,30 @@ namespace heist
         public Bank(int userDifficulty)
         {
             this.Difficulty = userDifficulty;
+        }
+        public void printReport()
+        {
+            Dictionary<string, int> scores = new Dictionary<string, int>()
+            {
+                {"Alarm", AlarmScore},
+                {"Vault", VaultScore},
+                {"Security", SecurityGuardScore}
+            };
+
+            var sorted = from score in scores
+                        orderby score.Value
+                        select score;
+
+            string mostSecure = (from score in sorted
+                                 select score.Key).First();
+
+            string leastSecure = (from score in sorted
+                                  select score.Key).Last();
+
+            Console.WriteLine($"Most Secure: {mostSecure}");
+            Console.WriteLine($"Least Secure: {leastSecure}\n");
+
+
         }
     }
 }
